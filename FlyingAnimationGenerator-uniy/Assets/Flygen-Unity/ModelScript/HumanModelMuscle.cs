9e7cae7667c9d77b8d89072e1bb5b945b01fc3e4
sakata yusuke <xxxaaaxxxcsharp@gmail.com>
2017-07-05 04:56:04 +0900

    筋骨格モデルの基礎部分実装

---
+++ FlyingAnimationGenerator-uniy/Assets/Flygen-Unity/ModelScript/HumanModelMuscle.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanModelMuscle : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _leftTransform;
+    [SerializeField]
+    private Rigidbody _leftRigidbody;
+
+    [SerializeField]
+    private Transform _rightTransform;
+    [SerializeField]
+    private Rigidbody _rightRigidbody;
+
+
+    private float _startDistance;
+    private float _springPower;
+    private float _lengthChange;
+    private float _additionalPower;
+
+    void Start()
+    {
+        _startDistance = GetDistance().magnitude;
+    }
+
+    void Update()
+    {
+        var distance = GetDistance();
+        var dir = distance.normalized;
+        var targetDistance = _startDistance + _lengthChange;
+        var pullPower = (distance.magnitude - targetDistance) * _springPower + _additionalPower;
+
+        _rightRigidbody.AddForceAtPosition(dir * pullPower * -1, _rightRigidbody.transform.position);
+        _leftRigidbody.AddForceAtPosition(dir * pullPower, _leftTransform.transform.position);
+    }
+
+    Vector3 GetDistance()
+    {
+        return _rightRigidbody.transform.position - _leftTransform.transform.position;
+    }
+
+    public void SetSpringPower(float power)
+    {
+        _springPower = power;
+    }
+
+    public void SetLengthChange(float length)
+    {
+        _lengthChange = length;
+    }
+    public void SetAdditionalPower(float power)
+    {
+        _additionalPower = power;
+    }
+}
--- FlyingAnimationGenerator-uniy/Assets/Flygen-Unity/ModelScript/MuscleskeltalWalkerModel.cs
+++ FlyingAnimationGenerator-uniy/Assets/Flygen-Unity/ModelScript/MuscleskeltalWalkerModel.cs
@@ -2,35 +2,109 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class MuscleskeltalWalkerModel : IGaRunningModel
+public class MuscleskeltalWalkerModel : MonoBehaviour, IGaRunningModel
 {
-    public void InitValue(List<float> value)
+    [SerializeField]
+    private List<HumanModelMuscle> muscles;
+    [SerializeField]
+    private List<HumanModelMuscle> mirrorMuscles;
+
+    [SerializeField]
+    private int _sectionCount = 4;
+    [SerializeField]
+    private float _sectionTime = 0.15f;
+
+    [SerializeField]
+    private float _springPowerMax = 4000.0f;
+    [SerializeField]
+    private float _additionalPowerMax = 100.0f;
+    [SerializeField]
+    private float _lengthChangeMax = 0.1f;
+
+    private List<float> _rawData;
+    private bool _isEnd;
+    private float _evaluate;
+    private float _count;
+
+    private List<List<float>> powerList;
+    public void InitValue(List<float> data)
     {
-        throw new System.NotImplementedException();
+        _rawData = data;
+        int index = 0;
+        for (int i = 0; i < muscles.Count; i++)
+        {
+            muscles[i].SetSpringPower(data[index] * _springPowerMax);
+            muscles[i].SetLengthChange(data[index + 1] * _lengthChangeMax);
+            mirrorMuscles[i].SetSpringPower(data[index] * _springPowerMax);
+            mirrorMuscles[i].SetLengthChange(data[index + 1] * _lengthChangeMax);
+            index += 2;
+        }
+
+        powerList = new List<List<float>>();
+        for (int i = 0; i < _sectionCount; i++)
+        {
+            var target = new List<float>();
+            for (int j = 0; j < muscles.Count; j++)
+            {
+                target.Add(data[index] * _additionalPowerMax);
+                index++;
+            }
+            powerList.Add(target);
+        }
+
+        var collisionGround = GetComponentsInChildren<TaggedObjectCollisionDetecter>();
+        foreach (var a in collisionGround)
+            a.OnEnter = DoEvaluate;
+    }
+
+    void Update()
+    {
+        _count += Time.deltaTime;
+
+        if (powerList == null)
+            return;
+
+        var sectionNum = (int)(_count / _sectionTime) % _sectionCount;
+        var isMirror = (int)(_count / _sectionTime) % (_sectionCount * 2) / _sectionCount == 1;
+
+        for (int i = 0; i < muscles.Count; i++)
+        {
+            muscles[i].SetAdditionalPower(powerList[sectionNum][i] * (isMirror ? -1 : 1));
+            mirrorMuscles[i].SetAdditionalPower(powerList[sectionNum][i] * (isMirror ? 1 : -1));
+        }
+    }
+
+    void DoEvaluate()
+    {
+        if (_isEnd == false)
+        {
+            _isEnd = true;
+            _evaluate = _count;
+        }
     }
 
     public int GetDataLength()
     {
-        throw new System.NotImplementedException();
+        return muscles.Count * (_sectionCount + 2);
     }
 
     public bool IsEnd()
     {
-        throw new System.NotImplementedException();
+        return _isEnd;
     }
 
     public float GetEvaluate()
     {
-        throw new System.NotImplementedException();
+        return _evaluate;
     }
 
     public void Erase()
     {
-        throw new System.NotImplementedException();
+        Destroy(gameObject);
     }
 
     public List<float> GetData()
     {
-        throw new System.NotImplementedException();
+        return _rawData;
     }
 }
